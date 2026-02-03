98f0733b6c4f4f393ea6fe0d7a633b45e872f348
henilpatel5427-creator <henilpatel5427@gmail.com>
2026-02-03 12:32:19 +0530

    Add files via upload

---
+++ sql/SQL2.sql
@@ -0,0 +1,33 @@
+﻿use student ;
+
+--1️ Count total number of student
+
+SELECT COUNT (name) AS total_student
+FROM students;
+
+--2️ Find average marks of students
+
+SELECT AVG(marks) AS average_marks
+FROM students;
+
+--3️ Find highest and lowest marks
+
+SELECT 
+    MAX(marks) AS highest_marks,
+    MIN(marks) AS lowest_marks
+FROM students;
+
+--4️ Find department-wise average marks
+
+SELECT department, AVG(marks) AS avg_marks
+FROM students
+GROUP BY department;
+
+--5️ Display departments where average marks > 70
+
+SELECT department, AVG(marks) AS avg_marks
+FROM students
+GROUP BY department
+HAVING AVG(marks) > 70;
+
+
