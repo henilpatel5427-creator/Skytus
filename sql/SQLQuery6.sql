ab660817e42f352c34c77fef95a35276a26f8823
henilpatel5427-creator <henilpatel5427@gmail.com>
2026-02-03 12:36:27 +0530

    Add files via upload

---
+++ sql/SQLQuery6.sql
@@ -0,0 +1,56 @@
+﻿--1️ Create Database
+
+CREATE DATABASE statebank_db;
+USE statebank_db;
+
+--2️ Create accounts Table
+
+CREATE TABLE accounts (
+    acc_id INT PRIMARY KEY,
+    acc_name VARCHAR(50),
+    balance INT
+);
+
+--3 Insert Initial Records
+
+INSERT INTO accounts VALUES
+(101, 'Henil', 450000),
+(102, 'Urva', 900000);
+
+--4️ Start Transaction + Insert + Rollback
+
+BEGIN TRANSACTION;
+
+INSERT INTO accounts VALUES (103, 'Darshil', 40000);
+
+ROLLBACK;
+
+--5️ Start Transaction + Insert + Commit
+
+BEGIN TRANSACTION;
+
+INSERT INTO accounts VALUES (103, 'Darshil', 40000);
+
+COMMIT;
+
+--6️ Transfer Money Using Transaction
+
+BEGIN TRANSACTION;
+
+UPDATE accounts
+SET balance = balance - 5000
+WHERE acc_id = 101;
+
+UPDATE accounts
+SET balance = balance + 5000
+WHERE acc_id = 102;
+
+COMMIT;
+
+--7️ Final Data
+
+SELECT * FROM accounts;
+
+
+
+
